d2e9e1aade31f3a6dc169f81d76e4fbd3767a2a7
Jon Lee <jonlee@nichols-ve503-0472.apn.wlan.upenn.edu>
2017-11-19 02:39:13 -0500

    basic raymarching with equations

--- src/shaders/deferred.frag.glsl.js
+++ src/shaders/deferred.frag.glsl.js
@@ -18,6 +18,14 @@ export default function(params) {
 
   out vec4 out_Color;
 
+  #define ABSORBTION 0.006
+  #define SCATTERING 0.009
+  #define EXTINCTION ABSORBTION + SCATTERING
+  #define DENSITY 0.5
+  #define PI 3.14159265
+
+  #define NUM_STEPS 100 
+
   struct Light {
     vec3 position;
     float radius;
@@ -63,6 +71,24 @@ export default function(params) {
     }
   }
 
+  float transmittance(vec3 p1, vec3 p2)
+  {
+    float exponent = -EXTINCTION * DENSITY * distance(p1, p2);
+
+    return exp(exponent);
+  }
+
+  float phaseFunction(vec3 wo, vec3 wi, float g)
+  {
+    float dot = dot(wo, wi);
+    float cosTheta = cos(dot);
+
+    float num = 1.0f - (g * g);
+    float denom = 4.0f * PI * pow(1.0 + g * g - 2.0 * g * cosTheta, 1.5);
+
+    return num / denom;
+  }
+
   void main() {
     // 2 COMPONENT NORMALS:
     // vec4 gb0 = texture(u_gbuffers[0], v_uv);
@@ -71,77 +97,142 @@ export default function(params) {
     // vec3 albedo = gb1.rgb;
     // vec3 normal = vec3(gb0.w, gb1.w, sqrt(abs(1.0 - gb0.w * gb0.w - gb1.w * gb1.w)));// z2 = 1 - x2 - y2..
 
+    // Get position, color, and normal information from G-Buffer
     vec3 v_position = texture(u_gbuffers[0], v_uv).xyz;
     vec3 albedo = texture(u_gbuffers[1], v_uv).xyz;
     vec3 normal = texture(u_gbuffers[2], v_uv).xyz;
 
     vec3 fragColor = vec3(0.0);
 
-    ivec3 clusterPos = ivec3(
-      int(gl_FragCoord.x / u_screenW * float(${params.xSlices})),
-      int(gl_FragCoord.y / u_screenH * float(${params.ySlices})),
-      int((-(u_viewMatrix * vec4(v_position,1.0)).z - u_camN) / (u_camF - u_camN) * float(${params.zSlices}))
-    );
-    
-    // optimize z using non linear scale once linear works..
-    // show perf. comparison..
-
-    // use UnpackLight() logic to read lightIdx, and then use UnpackLight() to read light from that idx..
-
-    int clusterIdx = clusterPos.x + clusterPos.y * ${params.xSlices} + clusterPos.z * ${params.xSlices} * ${params.ySlices};
-    int clusterWidth = ${params.xSlices} * ${params.ySlices} * ${params.zSlices};
-    int clusterHeight = int(float(${params.maxLights}+1) / 4.0) + 1;
-    float clusterU = float(clusterIdx + 1) / float(clusterWidth + 1); // like u in UnpackLight()..
-
-    int numLights = int(texture(u_clusterbuffer, vec2(clusterU, 0.0)).x); // clamp to max lights in scene if this misbehaves..
-
     // DIRECTIONAL LIGHT - SUN
     vec3 sunDir = normalize(vec3(1.0, 0.5, 1.0));
     vec3 sunCol = vec3(0.5, 0.5, 0.4);
     fragColor += albedo * sunCol * max(dot(sunDir, normal), 0.05);
 
-    for (int i = 0; i < ${params.numLights}; i++) {
-      if(i >= numLights) {
-        break;
+    //-- Naive Volumetric Ray March
+    // Make a ray from the camera to the point in world space.
+    vec3 rayOrigin    = u_camPos;
+    vec3 rayDirection = normalize(v_position - rayOrigin);
+
+    float rayLength = length(v_position - rayOrigin);
+    float stepSize = rayLength / float(NUM_STEPS);
+
+    vec3 step = rayDirection * stepSize;
+
+    // float EXTINCTION = ABSORBTION + SCATTERING;
+    float pmAlbedo = SCATTERING / EXTINCTION;
+
+    vec3 fog = vec3(0.0);
+    for(float i = 0.0; i <= rayLength; i += stepSize) {
+      // Get ray marched point
+      vec3 p = rayOrigin + (i * rayDirection);
+
+      // Find transmittance from camera to ray marched point and add to overall
+      float tr = transmittance(rayOrigin, p);
+
+      // Solve Lscat
+      // TODO: Loop through all the lights
+      // TODO: Check for occlusion
+
+      // Just use the sun as the only light source for now
+      vec3 Li = vec3(0.0);
+      if(dot(normal, sunDir) > 0.0) {
+        Li = sunCol;
       }
 
-      int clusterPixel = int(float(i+1) / 4.0); // FIXED BUG: offset by 1
-      float clusterV = float(clusterPixel+1) / float(clusterHeight+1);
-      vec4 texel = texture(u_clusterbuffer, vec2(clusterU, clusterV));
-      int lightIdx;
-      int clusterPixelComponent = (i+1) - (clusterPixel * 4);
-      if (clusterPixelComponent == 0) {
-          lightIdx = int(texel[0]);
-      } else if (clusterPixelComponent == 1) {
-          lightIdx = int(texel[1]);
-      } else if (clusterPixelComponent == 2) {
-          lightIdx = int(texel[2]);
-      } else if (clusterPixelComponent == 3) {
-          lightIdx = int(texel[3]);
-      }
+      vec3 sum = phaseFunction(rayDirection, sunDir, -0.5) * transmittance(rayOrigin, sunDir) * Li;
 
-      // shading
-      Light light = UnpackLight(lightIdx);
-      float lightDistance = distance(light.position, v_position);
-      vec3 L = (light.position - v_position) / lightDistance;
+      vec3 Lscat = pmAlbedo * sum;
 
-      float lightIntensity = cubicGaussian(2.0 * lightDistance / light.radius);
-      float lambertTerm = max(dot(L, normal), 0.0);
-
-      float specular = 0.0;
-      // blinn-phong shading... https://en.wikipedia.org/wiki/Blinn%E2%80%93Phong_shading_model
-      vec3 viewDir = normalize(u_camPos - v_position);
-      vec3 halfDir = normalize(L + viewDir);
-      float specAngle = max(dot(halfDir, normal), 0.0);
-      specular = pow(specAngle, 100.0); // 100 -> shininess
-
-      fragColor += (albedo + vec3(specular)) * lambertTerm * light.color * vec3(lightIntensity);
+      // Accumulate 
+      fog += tr * EXTINCTION * Lscat;
     }
 
     const vec3 ambientLight = vec3(0.025);
-    fragColor += albedo * ambientLight;
+    fragColor += (transmittance(rayOrigin, v_position) * albedo * ambientLight) + fog;
 
     out_Color = vec4(fragColor, 1.0);
   }
+
+  // void main() {
+  //   // 2 COMPONENT NORMALS:
+  //   // vec4 gb0 = texture(u_gbuffers[0], v_uv);
+  //   // vec4 gb1 = texture(u_gbuffers[1], v_uv);
+  //   // vec3 v_position = gb0.xyz;
+  //   // vec3 albedo = gb1.rgb;
+  //   // vec3 normal = vec3(gb0.w, gb1.w, sqrt(abs(1.0 - gb0.w * gb0.w - gb1.w * gb1.w)));// z2 = 1 - x2 - y2..
+
+  //   vec3 v_position = texture(u_gbuffers[0], v_uv).xyz;
+  //   vec3 albedo = texture(u_gbuffers[1], v_uv).xyz;
+  //   vec3 normal = texture(u_gbuffers[2], v_uv).xyz;
+
+  //   vec3 fragColor = vec3(0.0);
+
+  //   ivec3 clusterPos = ivec3(
+  //     int(gl_FragCoord.x / u_screenW * float(${params.xSlices})),
+  //     int(gl_FragCoord.y / u_screenH * float(${params.ySlices})),
+  //     int((-(u_viewMatrix * vec4(v_position,1.0)).z - u_camN) / (u_camF - u_camN) * float(${params.zSlices}))
+  //   );
+    
+  //   // optimize z using non linear scale once linear works..
+  //   // show perf. comparison..
+
+  //   // use UnpackLight() logic to read lightIdx, and then use UnpackLight() to read light from that idx..
+
+  //   int clusterIdx = clusterPos.x + clusterPos.y * ${params.xSlices} + clusterPos.z * ${params.xSlices} * ${params.ySlices};
+  //   int clusterWidth = ${params.xSlices} * ${params.ySlices} * ${params.zSlices};
+  //   int clusterHeight = int(float(${params.maxLights}+1) / 4.0) + 1;
+  //   float clusterU = float(clusterIdx + 1) / float(clusterWidth + 1); // like u in UnpackLight()..
+
+  //   int numLights = int(texture(u_clusterbuffer, vec2(clusterU, 0.0)).x); // clamp to max lights in scene if this misbehaves..
+
+  //   // DIRECTIONAL LIGHT - SUN
+  //   vec3 sunDir = normalize(vec3(1.0, 0.5, 1.0));
+  //   vec3 sunCol = vec3(0.5, 0.5, 0.4);
+  //   fragColor += albedo * sunCol * max(dot(sunDir, normal), 0.05);
+
+  //   for (int i = 0; i < ${params.numLights}; i++) {
+  //     if(i >= numLights) {
+  //       break;
+  //     }
+
+  //     int clusterPixel = int(float(i+1) / 4.0); // FIXED BUG: offset by 1
+  //     float clusterV = float(clusterPixel+1) / float(clusterHeight+1);
+  //     vec4 texel = texture(u_clusterbuffer, vec2(clusterU, clusterV));
+  //     int lightIdx;
+  //     int clusterPixelComponent = (i+1) - (clusterPixel * 4);
+  //     if (clusterPixelComponent == 0) {
+  //         lightIdx = int(texel[0]);
+  //     } else if (clusterPixelComponent == 1) {
+  //         lightIdx = int(texel[1]);
+  //     } else if (clusterPixelComponent == 2) {
+  //         lightIdx = int(texel[2]);
+  //     } else if (clusterPixelComponent == 3) {
+  //         lightIdx = int(texel[3]);
+  //     }
+
+  //     // shading
+  //     Light light = UnpackLight(lightIdx);
+  //     float lightDistance = distance(light.position, v_position);
+  //     vec3 L = (light.position - v_position) / lightDistance;
+
+  //     float lightIntensity = cubicGaussian(2.0 * lightDistance / light.radius);
+  //     float lambertTerm = max(dot(L, normal), 0.0);
+
+  //     float specular = 0.0;
+  //     // blinn-phong shading... https://en.wikipedia.org/wiki/Blinn%E2%80%93Phong_shading_model
+  //     vec3 viewDir = normalize(u_camPos - v_position);
+  //     vec3 halfDir = normalize(L + viewDir);
+  //     float specAngle = max(dot(halfDir, normal), 0.0);
+  //     specular = pow(specAngle, 100.0); // 100 -> shininess
+
+  //     fragColor += (albedo + vec3(specular)) * lambertTerm * light.color * vec3(lightIntensity);
+  //   }
+
+  //   const vec3 ambientLight = vec3(0.025);
+  //   fragColor += albedo * ambientLight;
+
+  //   out_Color = vec4(fragColor, 1.0);
+  // }
   `;
 }

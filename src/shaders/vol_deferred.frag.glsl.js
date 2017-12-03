55644ba548fdc0920525b802acd6642905fd26ba
Jon Lee <jonlee@huntsman-ve508-0546.apn.wlan.upenn.edu>
2017-12-03 18:18:58 -0500

    fixed shadow maps

--- src/shaders/vol_deferred.frag.glsl.js
+++ src/shaders/vol_deferred.frag.glsl.js
@@ -50,140 +50,158 @@ export default function(params) {
 
   #define NUM_STEPS 100
 
-  void main() {
-    const vec3 ambientLight = vec3(0.025);
+  vec3 shadowMap(vec3 pos, vec3 nor, vec3 col)
+  {
+    vec4 position       = u_viewProjectionMatrix * vec4(pos, 1.0);
+    vec4 lightPosition  = u_lightViewProjectionMatrix * vec4(pos, 1.0);
+    // Remove some shadow acne
+    lightPosition.z -= 0.007;
 
-    // Get position, color, and normal information from G-Buffer
-    vec3 v_position = texture(u_gbuffers[0], v_uv).xyz;
-    vec3 albedo = texture(u_gbuffers[1], v_uv).xyz;
-    vec3 normal = texture(u_gbuffers[2], v_uv).xyz;
-    vec3 volCol = texture(u_volPassBuffer, v_uv).xyz;
+    // Get the light direction from the point to the light
+    vec4 lightDir     = normalize(vec4(0.0,0.0,0.0,1.0) - lightPosition);
+    // vec4 lightDir     = normalize(position - lightPosition);
+    vec3 shadowCoord  = (lightPosition.xyz / lightPosition.w) / 2.0 + 0.5;
 
-    vec4 vv_lightPosition = u_lightViewProjectionMatrix * vec4(v_position, 1.0);
-    vec4 vv_lightDir = normalize(vec4(0.0,0.0,0.0,1.0) - vec4(0.5, 4, 0.4, 1.0));
-    float dotprod = dot(vv_lightDir.xyz, normal);
-    vec3 shadowCoord = (vv_lightPosition.xyz/vv_lightPosition.w)/2.0 + 0.5;
-
-    vec4 rgbaDepth = texture(u_shadowMap, shadowCoord.xy);
-    float depth = rgbaDepth.r; // Retrieve the z-value from R
-    float visibility = (shadowCoord.z > depth+0.005)? 0.3 : 1.0;
+    vec3 sunCol       = vec3(0.5, 0.5, 0.4);
+    vec4 rgbaDepth    = texture(u_shadowMap, shadowCoord.xy);
+    float depth       = rgbaDepth.r; // Retrieve the z-value from R
+    float visibility  = (shadowCoord.z > depth+0.005)? 0.3 : 1.0;
     // out_Color = vec4(shadowCoord, 1.0);
-    albedo *= 10.0 * vec3(1.0,1.0,1.0) * max(dot(vv_lightDir.xyz, normal), 0.05);
-    out_Color = vec4(albedo * visibility, 1.0);
+    float dotprod     = dot(lightDir.xyz, nor);
+    vec3 albedo       = col * sunCol * max(dotprod, 0.05);
+    // out_Color      = vec4(albedo * visibility, 1.0);    
+
+    return albedo * visibility;
   }
 
-  // struct Light {
-  //   vec3 position;
-  //   float radius;
-  //   vec3 color;
-  // };
+  struct Light {
+    vec3 position;
+    float radius;
+    vec3 color;
+  };
+  
+  float ExtractFloat(sampler2D textureToSample, int textureWidth, int textureHeight, int index, int component) {
+    float u = float(index + 1) / float(textureWidth + 1);
+    int pixel = component / 4;
+    float v = float(pixel + 1) / float(textureHeight + 1);
+    vec4 texel = texture(textureToSample, vec2(u, v));
+    int pixelComponent = component - pixel * 4;
+    if (pixelComponent == 0) {
+      return texel[0];
+    } else if (pixelComponent == 1) {
+      return texel[1];
+    } else if (pixelComponent == 2) {
+      return texel[2];
+    } else if (pixelComponent == 3) {
+      return texel[3];
+    }
+  }
 
-  // float ExtractFloat(sampler2D textureToSample, int textureWidth, int textureHeight, int index, int component) {
-  //   float u = float(index + 1) / float(textureWidth + 1);
-  //   int pixel = component / 4;
-  //   float v = float(pixel + 1) / float(textureHeight + 1);
-  //   vec4 texel = texture(textureToSample, vec2(u, v));
-  //   int pixelComponent = component - pixel * 4;
-  //   if (pixelComponent == 0) {
-  //     return texel[0];
-  //   } else if (pixelComponent == 1) {
-  //     return texel[1];
-  //   } else if (pixelComponent == 2) {
-  //     return texel[2];
-  //   } else if (pixelComponent == 3) {
-  //     return texel[3];
-  //   }
-  // }
+  Light UnpackLight(int index) {
+    Light light;
+    float u = float(index + 1) / float(${params.numLights + 1});
+    vec4 v1 = texture(u_lightbuffer, vec2(u, 0.0));
+    vec4 v2 = texture(u_lightbuffer, vec2(u, 0.5));
+    light.position = v1.xyz;
+    light.radius = ExtractFloat(u_lightbuffer, ${params.numLights}, 2, index, 3);
+    light.color = v2.rgb;
+    return light;
+  }
 
-  // Light UnpackLight(int index) {
-  //   Light light;
-  //   float u = float(index + 1) / float(${params.numLights + 1});
-  //   vec4 v1 = texture(u_lightbuffer, vec2(u, 0.0));
-  //   vec4 v2 = texture(u_lightbuffer, vec2(u, 0.5));
-  //   light.position = v1.xyz;
-  //   light.radius = ExtractFloat(u_lightbuffer, ${params.numLights}, 2, index, 3);
-  //   light.color = v2.rgb;
-  //   return light;
-  // }
+  // Cubic approximation of gaussian curve so we falloff to exactly 0 at the light radius
+  float cubicGaussian(float h) {
+    if (h < 1.0) {
+      return 0.25 * pow(2.0 - h, 3.0) - pow(1.0 - h, 3.0);
+    } else if (h < 2.0) {
+      return 0.25 * pow(2.0 - h, 3.0);
+    } else {
+      return 0.0;
+    }
+  }
 
-  // // Cubic approximation of gaussian curve so we falloff to exactly 0 at the light radius
-  // float cubicGaussian(float h) {
-  //   if (h < 1.0) {
-  //     return 0.25 * pow(2.0 - h, 3.0) - pow(1.0 - h, 3.0);
-  //   } else if (h < 2.0) {
-  //     return 0.25 * pow(2.0 - h, 3.0);
-  //   } else {
-  //     return 0.0;
-  //   }
-  // }
-
-  // float transmittance(vec3 p1, vec3 p2)
-  // {
+  float transmittance(vec3 p1, vec3 p2)
+  {
     
-  //   float exponent = -EXTINCTION * texture(u_volBuffer, p2.xxx/u_volSize/4.0).x/255.0/*DENSITY*/ * distance(p1, p2);
+    float exponent = -EXTINCTION * texture(u_volBuffer, p2.xxx/u_volSize/4.0).x/255.0/*DENSITY*/ * distance(p1, p2);
 
-  //   return exp(exponent);
-  // }
+    return exp(exponent);
+  }
 
-  // float phaseFunction(vec3 wo, vec3 wi, float g)
-  // {
-  //   float dot = dot(wo, wi);
-  //   float cosTheta = cos(dot);
+  float phaseFunction(vec3 wo, vec3 wi, float g)
+  {
+    float dot = dot(wo, wi);
+    float cosTheta = cos(dot);
 
-  //   float num = 1.0f - (g * g);
-  //   float denom = 4.0f * PI * pow(1.0 + g * g - 2.0 * g * cosTheta, 1.5);
+    float num = 1.0f - (g * g);
+    float denom = 4.0f * PI * pow(1.0 + g * g - 2.0 * g * cosTheta, 1.5);
 
-  //   return num / denom;
-  // }
+    return num / denom;
+  }
 
-  // float phaseFunction()
-  // {
-  //     return 1.0/(4.0*PI);
-  // }
+  float phaseFunction()
+  {
+      return 1.0 / (4.0 * PI);
+  }
 
-  // vec2 intersectCube(vec3 p, vec3 dir)
-  // {
-  //   float tNear = -1000.0;
-  //   float tFar  = 1000.0;
+  vec2 intersectCube(vec3 p, vec3 dir)
+  {
+    float tNear = -1000.0;
+    float tFar  = 1000.0;
 
-  //   float min = -u_volSize / 2.0;
-  //   float max = u_volSize / 2.0;
+    float min = -u_volSize / 2.0;
+    float max = u_volSize / 2.0;
 
-  //   for(int i = 0; i < 3; i++) {
-  //     // X-Slab
-  //     if(dir[i] == 0.0) {
-  //       if(p[i] < min || p[i] > max) {
-  //         // miss
-  //         return vec2(2000.0, 2000.0);
-  //       }
-  //     }
+    for(int i = 0; i < 3; i++) {
+      // X-Slab
+      if(dir[i] == 0.0) {
+        if(p[i] < min || p[i] > max) {
+          // miss
+          return vec2(2000.0, 2000.0);
+        }
+      }
 
-  //     float t0 = (min - p[i]) / dir[i];
-  //     float t1 = (max - p[i]) / dir[i];
+      float t0 = (min - p[i]) / dir[i];
+      float t1 = (max - p[i]) / dir[i];
 
-  //     if(t0 > t1) {
-  //       // swap
-  //       float t = t0;
-  //       t0 = t1;
-  //       t1 = t;
-  //     }
+      if(t0 > t1) {
+        // swap
+        float t = t0;
+        t0 = t1;
+        t1 = t;
+      }
 
-  //     if(t0 > tNear) {
-  //       tNear = t0;
-  //     }
+      if(t0 > tNear) {
+        tNear = t0;
+      }
 
-  //     if(t1 < tFar) {
-  //       tFar = t1;
-  //     }
-  //   }
+      if(t1 < tFar) {
+        tFar = t1;
+      }
+    }
 
-  //   if(tNear > tFar) {
-  //     // miss 
-  //     return vec2(2000.0, 2000.0);
-  //   }
+    if(tNear > tFar) {
+      // miss 
+      return vec2(2000.0, 2000.0);
+    }
 
-  //   return vec2(tNear, tFar);
-  // }
+    return vec2(tNear, tFar);
+  }
+
+  void main() {
+    // Get position, color, and normal information from G-Buffer
+    vec3 v_position = texture(u_gbuffers[0], v_uv).xyz;
+    vec3 albedo     = texture(u_gbuffers[1], v_uv).xyz;
+    vec3 normal     = texture(u_gbuffers[2], v_uv).xyz;
+    vec3 volCol     = texture(u_volPassBuffer, v_uv).xyz;
+    vec3 shadow     = shadowMap(v_position, normal, albedo);
+   
+    const vec3 ambientLight = vec3(0.025);
+    vec3 fragColor = vec3(0.0, 0.0, 0.0);
+    
+    fragColor += shadow;
+    
+    out_Color = vec4(fragColor, 1.0);
+  }
 
   // void main() {
   //   const vec3 ambientLight = vec3(0.025);

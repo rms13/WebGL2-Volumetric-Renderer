5c883c85e948605355b37903e79c8c5c4dc689b3
Jon Lee <agentlee927@gmail.com>
2017-12-08 15:22:58 -0500

    fixed shadow map issue on windows. removed os/browser detection

--- src/shaders/shadow.vert.glsl
+++ src/shaders/shadow.vert.glsl
@@ -4,40 +4,16 @@ precision highp float;
 uniform mat4 u_viewProjectionMatrix;
 
 in vec3 a_position;
+in vec3 a_normal;
 in vec2 a_uv;
 
 out vec3 v_position;
+out vec3 v_normal;
 out vec2 v_uv;
 
 void main() {
     gl_Position = u_viewProjectionMatrix * vec4(a_position, 1.0);
     v_position  = a_position;
+    v_normal    = a_normal;
     v_uv        = a_uv;
 }
-
-
-// #version 300 es
-// precision highp float;
-
-// in vec3 a_position;
-
-// // This will be from the light's perpsective
-// uniform mat4 u_viewProjectionMatrix;
-// // Take the point back to its model
-// // uniform mat4 u_invViewProjectionMatrix;
-
-// out vec3 color;
-// // out vec3 v_pos;
-// // out vec2 v_uv;
-
-// void main() 
-// {
-//     gl_Position = u_viewProjectionMatrix * vec4(a_position, 1.0);
-
-//     color = a_position;
-
-//     // gl_Position = u_viewProjectionMatrix * vec4(a_position, 1.0);
-    
-//     // v_pos = a_position;
-//     // v_uv = a_position.xy * 0.5 + 0.5;
-// }
